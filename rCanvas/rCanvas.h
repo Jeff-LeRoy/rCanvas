c24d86322bd9c2655c5178411dfc019b1173d42a
Jeffrey LeRoy <leroyjc@protonmail.com>
2022-01-17 18:07:36 -0500

    Add project files.

---
+++ rCanvas/rCanvas.h
@@ -0,0 +1,20 @@
+#pragma once
+class rCanvas
+{
+};
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
